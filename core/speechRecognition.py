5ca5c267d821257fb3d66bbcfa075fe741e1be5e
oncado86 <77399565+oncado86@users.noreply.github.com>
2024-01-07 20:42:42 +0300

    Add files via upload

--- core/speechRecognition.py
+++ core/speechRecognition.py
@@ -6,6 +6,7 @@
 # pip install SpeechRecognition
 # pip install requests
 """
+import threading
 import speech_recognition as sr  # type: ignore
 import requests
 
@@ -25,41 +26,61 @@ class SpeechRecognition:
     @category: Utilities
     """
 
+    def __init__(self) -> None:
+        self.r = sr.Recognizer()
+        self.text = None
+
     def recognize_speech(self) -> tuple[bool, str]:
         """
-        Recognizes speech using the Google Speech Recognition API.
+        Recognizes speech and returns a tuple indicating whether the speech was successfully recognized and the recognized speech text.
 
         Returns:
-            A tuple containing a boolean value indicating success or failure of speech recognition,
-            and a string representing the recognized speech.
-
-        Raises:
-            UnknownValueError: If the speech cannot be understood.
-            RequestError: If there is an error with the speech recognition request.
-
-
-        @category: Utilities
+            tuple[bool, str]: A tuple containing a boolean value indicating whether the speech was successfully recognized and a string representing the recognized speech text.
         """
-        r = sr.Recognizer()
 
         if not self.__is_connect_internet():
             return False, "Lütfen internet bağlantınızı kontrol ediniz"
 
-        with sr.Microphone() as source:
-            print("Dinliyorum...")
-            audio = r.listen(source)  # type: ignore
+        self.text = None
+        thread = threading.Thread(target=self.__listen)
+        thread.start()
+        thread.join(timeout=3.5)
+
+        if thread.is_alive():
+            thread.join(timeout=3)
+
+        if self.text:
+            return True, str(self.text)
+        else:
+            return False, "Ses anlaşılamadı veya zaman aşımı"
+
+    def __listen(self) -> None:
+        """
+        Listens for audio input using the microphone and converts it to text.
+
+        This method is used to listen for audio input from the microphone using the `sr.Microphone` class from the `speech_recognition` library. 
+        It then uses the `recognize_google` method of the `sr.Recognizer` class to convert the audio to text.
+
+        Parameters:
+            None
+
+        Return:
+            None
+
+        Raises:
+            - sr.UnknownValueError: If the audio cannot be recognized as speech.
+            - sr.RequestError: If there is an error making the speech recognition request.
+        """
 
         try:
-            text: str = r.recognize_google(
-                audio, language="tr-TR")  # type: ignore
-            print("Metin:", text)  # type: ignore
-            return True, text
-        except sr.UnknownValueError:
-            print("Ses anlaşılamadı")
-            return False, "Ses anlaşılmadı"
-        except sr.RequestError as e:
-            print("Hata:", e)
-            return False, f"Hata: {e}"
+            with sr.Microphone() as source:
+                audio = self.r.listen(source)
+
+            self.text = self.r.recognize_google(
+                audio, language="tr-TR")
+
+        except (sr.UnknownValueError, sr.RequestError):
+            self.text = None
 
     def __is_connect_internet(self) -> bool:
         """
